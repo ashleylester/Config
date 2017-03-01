60f68ec80083441116409f628fae72464546c471
Ashley Lester <ashleylester4@gmail.com>
2017-03-01 10:01:27 +0000

    update init

--- init.el
+++ init.el
@@ -1,52 +1,41 @@
-(require 'package) ;; You might already have this line
+(require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ace-window ample-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(package-initialize)
 
-;; then in your init you can load all of the themes
-;; without enabling theme (or just load one)
+(defvar my-packages '(better-defaults
+                      projectile
+                      clojure-mode
+                      cider
+                      ess
+                      magit
+                      ample-theme
+                      ace-window))
+
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
+(global-linum-mode t)
+(set-default-font "Crisp-12")
 (load-theme 'ample t t)
-(load-theme 'ample-flat t t)
-(load-theme 'ample-light t t)
-;; choose one to enable
 (enable-theme 'ample)
-;; (enable-theme 'ample-flat)
-;; (enable-theme 'ample-light)
-
-(set-frame-font "ProggyCleanTT-12")
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
+(mouse-wheel-progressive-speed nil)
 
-(add-to-list 'load-path "/home/ash/.emacs.d/lisp/powerline")
-
-(require 'powerline)
+(add-to-list 'load-path "c:/Users/ashley.lester/.emacs.d/lisp") ;non-elpa package dir
+(require 'powerline) ;install this manually
 (powerline-default-theme)
 
-(global-set-key (kbd "M-p") 'ace-window)
+(global-set-key (kbd "M-\\") 'ace-window)
 
 (defun set-window-width (n)
   "Set the selected window's width."
   (adjust-window-trailing-edge (selected-window) (- n (window-width)) t))
 
-(defun set-80-columns ()
-  "Set the selected window to 80 columns."
+(defun set-100-columns()
   (interactive)
-  (set-window-width 80))
+  (set-window-width 100))
 
-(global-set-key "\C-x~" 'set-80-columns)
-
+(global-set-key (kbd "\C-x ~") 'set-100-columns)
